ce60e50b1b958ac90eb1b5c869ced140e188b721
BensonGitau <bensonkamaugitau@gmail.com>
2024-05-03 14:12:00 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import {pointsForWord} from '../utils'
+
+describe("pointsForWord", ()=>{
+    it("Calculates the total points for a word( 1 point per vowel, 2 per constant)", ()=>{
+        const word= 'tEsT'
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,21 @@
 // Your code here
+/*export function pointsForWord(word){
+    let points= 0
+    let vowels = ["a", "e","i", "o","u"]
+    for(const char of word){
+        if(vowels.includes(char)){
+            points +=1
+        } else {
+            points +=2
+        }
+    }
+    return points
+}*/
+
+export function pointsForWord(word) {
+  let points = 0;
+  for (const char of word) {
+    points += /[aeiou]/i.test(char) ? 1 : 2;
+  }
+  return points;
+}
